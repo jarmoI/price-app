2a0b8e1eac1516c9557a6decae2ea050092d5c07
Michał Jarmoliński <michal.jarmolinski@anchorgroup.software>
2021-08-25 09:39:01 +0200

    cleaned files

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,37 +1,91 @@
 import React, { useState } from "react";
-import { ExpenseFormItem } from "./ExpenseFormItem";
 import "./ExpenseForm.css";
 
 export const ExpenseForm = (props) => {
-  const [formVisibility, setFormVisibility] = useState(false);
+  const [expenseTitle, setexpenseTitle] = useState("");
+  const [expenseAmount, setexpenseAmount] = useState("");
+  const [expenseDate, setexpenseDate] = useState("");
 
-  function showForm() {
-    setFormVisibility(true);
-  }
-  function hideForm() {
-    setFormVisibility(!formVisibility);
+  /*
+  const [userInput, setUserInput] = useState({
+    expenseTitle: '',
+    expenseAmount: '',
+    expenseDate: ''
+  });
+  */
+
+  function expenseTitleChangeHandler(event) {
+    setexpenseTitle(event.target.value);
+    /*
+    setUserInput({
+      ...prevState,
+      expenseTitle: event.target.value
+    })
+    setUserInput((prevState) => {
+      return {...prevState, expenseTitle: event.target.value}
+    })
+    */
   }
 
-  function saveExpenseDataHandler(data) {
-    const expenseData = {
-      ...data,
-      id: Math.random().toString()
-    }
-    props.onSaveExpenseDataHandler(expenseData);
-
+  function expenseAmountChangeHandler(event) {
+    setexpenseAmount(event.target.value);
   }
+
+  function expenseDateChangeHandler(event) {
+    setexpenseDate(event.target.value);
+  }
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const dataToSend = {
+      description: expenseTitle,
+      price: expenseAmount,
+      date: new Date(expenseDate),
+    };
+    console.log(dataToSend);
+    props.onSaveExpenseDataHandler(dataToSend);
+    setexpenseAmount("");
+    setexpenseDate("");
+    setexpenseTitle("");
+  };
 
   return (
     <div>
-      {formVisibility && <ExpenseFormItem onSaveExpenseData={saveExpenseDataHandler}/>}
-      <button onClick={showForm} type="submit">
-        Add expense
-      </button>
-      {formVisibility && (
-        <button type="button" onClick={hideForm}>
-          Cancel
-        </button>
-      )}
+      <form onSubmit={submitHandler}>
+        <div className="new-expense__controls">
+          <div className="new-expense__control">
+            <label>Title</label>
+            <input
+              type="text"
+              value={expenseTitle}
+              onChange={expenseTitleChangeHandler}
+            />
+          </div>
+          <div className="new-expense__control">
+            <label>Amount</label>
+            <input
+              type="number"
+              min="0.01"
+              step="0.01"
+              value={expenseAmount}
+              onChange={expenseAmountChangeHandler}
+            />
+          </div>
+          <div className="new-expense__control">
+            <label>Date</label>
+            <input
+              type="date"
+              min="2019-01-01"
+              max="2022-12-12"
+              value={expenseDate}
+              onChange={expenseDateChangeHandler}
+            />
+          </div>
+        </div>
+        <div className="new-expense__actions"></div>
+        <button type="button" onClick={props.onCancel}>Powrót</button>
+        <button type="submit">Dodaj</button>
+      </form>
     </div>
   );
 };
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,21 +1,65 @@
-import React from 'react';
-import {ExpenseForm} from './ExpenseForm'
-import './NewExpense.css';
+import React, { useState } from "react";
+import { ExpenseForm } from "./ExpenseForm";
+import "./NewExpense.css";
 
 export const NewExpense = (props) => {
+  const [formVisibility, setFormVisibility] = useState(false);
 
-  const saveExpenseData = (enteredExpense) => {
+  function showForm() {
+    setFormVisibility(true);
+  }
+  function hideForm() {
+    setFormVisibility(!formVisibility);
+  }
+
+  function saveExpenseDataHandler(data) {
     const expenseData = {
-      ...enteredExpense,
-      id: Math.random().toString()
-    }
-    console.log(expenseData);
-    props.onAddExpense(expenseData)
+      ...data,
+      id: Math.random().toString(),
+    };
+    props.onAddExpense(expenseData);
+  }
+
+  // const saveExpenseData = (enteredExpense) => {
+  //   const expenseData = {
+  //     ...enteredExpense,
+  //     id: Math.random().toString(),
+  //   };
+  //   console.log(expenseData);
+  //   props.onAddExpense(expenseData);
+  // };
+
+  {
+    /*
+    {formVisibility && (
+        <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+      )}
+   <button onClick={showForm} type="submit">
+    Add expense
+    </button>
+    {formVisibility && (
+      <button type="button" onClick={hideForm}>
+        Cancel
+      </button>
+    )}
+  */
   }
 
   return (
+    // <div className="new-expense">
+    // <button onClick={showForm} type="submit">
+    //   Add expense
+    //   </button>
+    //   { formVisibility && <ExpenseForm onSaveExpenseDataHandler={saveExpenseDataHandler} />}
+    //   </div>
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseDataHandler={saveExpenseData}/>
+      {!formVisibility && <button onClick={showForm}>Dodaj</button>}
+      {formVisibility && (
+        <ExpenseForm
+          onCancel={hideForm}
+          onSaveExpenseDataHandler={saveExpenseDataHandler}
+        />
+      )}
     </div>
   );
 };
